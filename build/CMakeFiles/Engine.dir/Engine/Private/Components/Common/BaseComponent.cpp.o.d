2bb430689d89a5091426e746244ed1e903d8fa25
UnderTheBedStudios <peteraymondiv@gmail.com>
2026-08-17 20:15:58 -0500

    Commit after .gitignore takes charge.

--- build/CMakeFiles/Engine.dir/Engine/Private/Components/Common/BaseComponent.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Engine.dir/Engine/Private/Components/Common/BaseComponent.cpp.o: \
- /mnt/GAEMS/Visual\ Studio\ Code/Zeyrixon/Engine/Private/Components/Common/BaseComponent.cpp \
- /usr/include/stdc-predef.h \
- /mnt/GAEMS/Visual\ Studio\ Code/Zeyrixon/Engine/../Engine/Public/Components/Common/BaseComponent.h \
- /mnt/GAEMS/Visual\ Studio\ Code/Zeyrixon/Engine/../Engine/Public/Base.h
--- build/CMakeFiles/Engine.dir/Engine/pch.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Engine.dir/Engine/pch.cpp.o: \
- /mnt/GAEMS/Visual\ Studio\ Code/Zeyrixon/Engine/pch.cpp \
- /usr/include/stdc-predef.h \
- /mnt/GAEMS/Visual\ Studio\ Code/Zeyrixon/Engine/pch.h \
- /mnt/GAEMS/Visual\ Studio\ Code/Zeyrixon/Engine/framework.h
