1bf2d85ffad9b1fb94347ffe58cd42b8ccc57c42
UnderTheBedStudios <peteraymondiv@gmail.com>
2026-09-16 20:52:21 -0500

    Fixed the OpenGL Clear Color to be the correct color.

--- Zeyrixon/src/Zeyrixon/Core/Application.cpp
+++ Zeyrixon/src/Zeyrixon/Core/Application.cpp
@@ -48,7 +48,7 @@ namespace Zeyrixon
     {
         while (m_Running)
         {
-            glClearColor(1, 1, 1, 1);
+            glClearColor(0.19, 0.19, 0.19, 1);
             glClear(GL_COLOR_BUFFER_BIT);
 
             for (Layer* layer : m_LayerStack)
--- ZeyrixonEditor/src/ZeyrixonEditor/Core/EditorLayer.cpp
+++ ZeyrixonEditor/src/ZeyrixonEditor/Core/EditorLayer.cpp
@@ -169,7 +169,7 @@ namespace Editor
     void EditorLayer::OnUpdate()
     {
         m_Framebuffer->Bind();
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Rendering goes here later
